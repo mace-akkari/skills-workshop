db7fadd9c08919032bf3432cec5d737fd316b343
Mace <maceplus@gmail.com>
2020-10-04 14:41:30 +0100

    excersise 2 complete

--- debugging_fizzbuzz/exercise_2/lib/cipher.rb
+++ debugging_fizzbuzz/exercise_2/lib/cipher.rb
@@ -1,7 +1,8 @@
 class Cipher
 
   def encode(plaintext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+    #error was using 'a'...'z', as ... dose not include the first and last letter
     ciphertext_chars = plaintext.chars.map do |char|
       (65 + cipher.find_index(char)).chr
     end
@@ -9,9 +10,9 @@ class Cipher
   end
 
   def decode(ciphertext, key)
-    cipher = key.chars.uniq + (('a'...'z').to_a - key.chars)
+    cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
     plaintext_chars = ciphertext.chars.map do |char|
-      cipher[65 - char.ord]
+      cipher[char.ord - 65]
     end
     plaintext_chars.join
   end
